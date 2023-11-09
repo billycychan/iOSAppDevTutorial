c06634d60a4b7b36012063643186c6c56c2128ac
Billy Chan <billycychan.work@gmail.com>
2023-11-09 02:27:43 -0500

    feat: created edit view

--- Scrumdinger/Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Scrumdinger/Views/DetailView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct DetailView: View {
     let scrum: DailyScrum
     
+    
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List {
             Section {
@@ -46,11 +49,34 @@ struct DetailView: View {
             
         }
         .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
 #Preview {
-    NavigationStack {
-        DetailView(scrum: DailyScrum.sampleData[0])
-    }
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
 }
